52eb4a373506ae8433847d017af5402447fb8d4b
gireyosmanov <girey345@gmail.com>
2023-10-15 18:38:25 +0300

    create button listener

--- app/src/main/java/com/example/madt_2_lab/MainActivity.kt
+++ app/src/main/java/com/example/madt_2_lab/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.madt_2_lab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupButtonClickListener()
+    }
+
+    private fun setupButtonClickListener() {
+        val editText = findViewById<EditText>(R.id.editText)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val text = editText.text.toString()
+            if (text.isEmpty()) {
+                showToastMessage("Please enter some text")
+                return@setOnClickListener
+            }
+
+            val result = calculateResult(text, spinner.selectedItem.toString())
+            textView.text = result.toString()
+        }
+    }
+
+    private fun showToastMessage(message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun calculateResult(text: String, selectedItem: String): Int {
+        return if ("Words" == selectedItem) {
+            TextCounter.countWords(text)
+        } else {
+            TextCounter.countCharacters(text)
+        }
     }
 }
